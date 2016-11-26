2ea5cf38b270c681daa465d31fc711a234971922
amisha <amisha.agarwal29@gmail.com>
2016-11-26 13:01:15 +0530

    BUGFIX

--- config/database.php
+++ config/database.php
@@ -54,15 +54,15 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', '127.0.0.1'),
             'database'  => env('DB_DATABASE', 'msritweb'),
-            'username'  => env('DB_USERNAME', 'jharvard'),
-            'password'  => env('DB_PASSWORD', 'crimson'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', 'root'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-            'unix_socket' => '/tmp/mysql.sock',
+            'unix_socket' => '/var/run/mysqld/mysqld.sock',
         ],
 
         'pgsql' => [
